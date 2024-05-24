ed408b667171618fd45caf602a93c17d667ca254
Haris Arif <harif3496@gmail.com>
2024-05-24 23:20:53 +0500

    php mail

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -52,6 +52,7 @@ class StudentController extends Controller
         $headers .= "Reply-To: ceo@edexceledu.com\r\n";
         $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
         
+        
         // Send email
         if (mail($to, $subject, $message, $headers)) {
             echo "Email sent successfully.";
