24eb8888ee081f0ce22daf245861b099f0f2f5da
Haris Arif <harif3496@gmail.com>
2024-05-27 16:56:32 +0500

    add auth

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,7 @@ Route::view('profile', 'profile')
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
