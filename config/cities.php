c4260fd275ecd34a359ef99f87904dd6cef24eae
Haris arif <harif3496@gmai.com>
2024-08-05 10:46:09 +0500

    update ciites

--- config/cities.php
+++ config/cities.php
@@ -4,7 +4,7 @@ return [
     'cities' => [
         'US' => ['New York', 'Los Angeles', 'Chicago'],
         'GB' => ['New York', 'Los Angeles', 'Chicago'],
-        'UAE' => ['Dubai', 'Abu Dhabi', 'Sharjah','Al Ain','Ajman','Ras Al Khaimah','Fujairah','Umm Al Quwain','Kalba','Madinat Zayed','Khor Fakkan','Dibba Al-Fujairah','Ruwais','	Ghayathi','Dhaid','	Jebel Ali','Liwa Oasis','Hatta','Ar-Rams','Dibba Al-Hisn','Al Jazirah Al Hamra'],
+        'UAE' => ['Dubai', 'Abu Dhabi', 'Sharjah','Al Ain','Ajman','Ras Al Khaimah','Fujairah','Umm Al Quwain'],
         // Other countries and their cities
     ],
 ];
