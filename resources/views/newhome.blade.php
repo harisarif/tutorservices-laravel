cfb318a7cda5ba98a2db766b8282ee0a6fecca1b
Adeelimran462 <sheikhadeel462@gmail.com>
2024-08-23 12:03:49 +0500

    responsive

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -407,14 +407,14 @@
                                         @foreach ($tutors as $item)
                                         <div class="ad-form">
                                             <div class="ad-img-card d-flex">
-                                                <div class="MD ">
+                                                <div class="MD col-5">
                                                     <img src="{{ asset('storage/' . $item->profileImage) }}" alt="..." class="img-thumbnail" style="max-width: 100%; height: 100px; width: 100px; border-radius: 70px;">
                                                 <div class="ad-icons">
 
                                                 </div >
                                                     <p class="mb-0 mx-1 fs-5" style="color:#42b979;">4.5 <i class="fa-solid fa-star"></i></p>
                                                 </div>
-                                                <div class="md-div col-9 d-none mt-2">
+                                                <div class="md-div col-9 d-none mt-2" style="margin-left: -5px;">
                                                         <span class="mb-div"><b>20 AED for 50 mintues</b>
                                                         </span>
 
