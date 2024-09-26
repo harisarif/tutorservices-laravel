af63b47e8dd5d7216a7ce568f55c328893718730
Adeelimran462 <sheikhadeel462@gmail.com>
2024-09-26 18:34:10 +0500

    footer

--- resources/views/basicsignup.blade.php
+++ resources/views/basicsignup.blade.php
@@ -78,26 +78,17 @@
     
     <footer class="row text-center text-lg-start justify-content-center m-0 p-0">
         
-        <div class="col-12 footer-bottom">
+        <div class="col-12 footer-bottom border-top">
             <ul class="p-3 text-center">
                 <li class="d-inline text-light">© Copyright 2024.</li>
-                <li class="d-inline text-light">All Rights Reserved.</li>
-                <li class="d-inline">
-                    <a class="text-decoration-none text-light" href="#">Privacy Policy</a>
-                </li>
-                <li class="d-inline text-light">|</li>
-                <li class="d-inline">
-                    <a class="text-decoration-none text-light" href="#">Terms & Conditions</a>
-                </li>
+                <li class="d-inline text-light">All Rights Reserved.</li>        
                 <li class="d-inline text-light">|</li>
                 <li class="d-inline text-light">Managed by</li>
                 <li class="d-inline">
-                    <a class="text-decoration-none text-light" href="#">Tecktrack</a>
+                    <a class="text-decoration-none text-light" href="https://techtrack.online/">Techtrack</a>
                 </li>
-                <li class="d-inline text-light">|</li>
-                <li class="d-inline">
-                    <a class="text-decoration-none text-light" href="#">FAQ</a>
-                </li>
+            
+                
             </ul>
         </div>
     </footer>
