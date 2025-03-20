ac1a0984f24ccf96bfc29605fd105fcb99311c60
husnainmehboob189 <husnainmehboob189@gmail.com>
2025-03-20 15:37:41 +0500

    design

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -639,7 +639,7 @@
                                                                             </video> -->
                                                                     </div>
                                                                     <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
-                                                                        <div class="modal-dialog modal-lg modal-dialog-centered"style="max-width:500px;">
+                                                                        <div class="modal-dialog modal-lg" id="pro1">
                                                                             <div class="modal-content">
                                                                                 <div class="modal-header"style="background-color: #1cc88a;">
                                                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="filter: invert(2);"></button>
